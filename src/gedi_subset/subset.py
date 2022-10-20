c5b3092130c71a1cb472756a14c9945a093897fe
Jamison French <jamisonjohnfrench@gmail.com>
2022-10-20 14:08:14 -0500

    chore: Pull Request changes

--- src/gedi_subset/subset.py
+++ src/gedi_subset/subset.py
@@ -41,7 +41,7 @@ class CMRHost(str, Enum):
     nasa = "cmr.earthdata.nasa.gov"
 
 
-logicals = {"L4A": "10.3334/ORNLDAAC/2056", "L2A": "10.5067/GEDI/GEDI02_A.002"}
+logical_doi = {"L4A": "10.3334/ORNLDAAC/2056", "L2A": "10.5067/GEDI/GEDI02_A.002"}
 
 LOGGING_FORMAT = "%(asctime)s [%(processName)s:%(name)s] [%(levelname)s] %(message)s"
 
@@ -172,11 +172,12 @@ def main(
     ),
     doi=typer.Option(
         ...,
+        callback=lambda value: logical_doi.get(value.upper(), value),
         help=(
             "Digital Object Identifier (DOI) of collection to subset"
             " (https://www.doi.org/)"
-            " Can be a specific DOI"
-            f" or one of these logical names: {', '.join(logicals.keys())}"
+            " Can be a specific DOI or one of these logical,"
+            f" case-insensitive names: {', '.join(logical_doi)}"
         ),
     ),
     cmr_host: CMRHost = typer.Option(
@@ -184,11 +185,11 @@ def main(
         help="CMR hostname",
     ),
     columns: str = typer.Option(
-        None,
+        ...,
         help="Comma-separated list of columns to select",
     ),
     query: str = typer.Option(
-        None,
+        ...,
         help="Boolean query expression to select rows",
     ),
     limit: int = typer.Option(
@@ -211,8 +212,6 @@ def main(
 ) -> None:
     logging_level = logging.DEBUG if verbose else logging.INFO
     set_logging_level(logging_level)
-
-    doi = logicals.get(doi, doi)
 
     os.makedirs(output_dir, exist_ok=True)
     dest = output_dir / "gedi_subset.gpkg"
