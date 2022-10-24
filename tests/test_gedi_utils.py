162bd59b4c78eabbfd4b4fa86a861e213c1138ad
Chuck Daniels <chuck@developmentseed.org>
2022-10-24 16:25:01 -0400

    feat: select columns from 2D datasets (#16)
    Fixes: #5, #7

--- tests/test_gedi_utils.py
+++ tests/test_gedi_utils.py
@@ -24,7 +24,17 @@ def fixture_path(filename: str) -> str:
         ({"sensitivity", "agbd"}, "agbd > 1 and l2_quality_flag == 1", 2),
         ({"sensitivity", "agbd", "agbd_se"}, "agbd_se > 3", 3),
         ({"agbd", "lat_lowestmode", "lon_lowestmode"}, "sensitivity >= 0.9", 4),
-        ({"landsat_treecover"}, "landsat_treecover > 60.0", 4),
+        ({"x_var0"}, "l2_quality_flag == 1 & sensitivity > 0.9", 2),
+        (
+            {"land_cover_data/landsat_treecover"},
+            "land_cover_data.landsat_treecover > 60.0",
+            4,
+        ),
+        (
+            {"land_cover_data/landsat_treecover"},
+            "`land_cover_data/landsat_treecover` > 60.0",
+            4,
+        ),
         ({"sensitivity", "agbd", "agbd_se"}, "sensitivity > 0.96", 2),
         ({"sensitivity", "agbd", "agbd_se"}, None, 4),
         ({"sensitivity"}, None, 4),
@@ -38,9 +48,15 @@ def test_subset_hdf5(
     n_expected_rows: int,
 ) -> None:
     with h5py.File(h5_path) as hdf5:
-        gdf = subset_hdf5(hdf5, aoi_gdf, columns, query)
+        gdf = subset_hdf5(hdf5, aoi_gdf, list(columns), query)
 
     expected_columns = columns | {"filename", "BEAM", "geometry"}
 
     assert set(gdf.columns) == expected_columns
     assert gdf.shape == (n_expected_rows, len(expected_columns))
+
+    # Make sure there are no NaN values.  If there is a NaN somewhere, that means that
+    # we either have a bug, or we didn't construct our fixture data correctly, so we
+    # should first verify the correctness of our fixture data, otherwise we might spend
+    # unnecessary time hunting down a non-existent bug.
+    assert gdf.notna().all(axis=None)
